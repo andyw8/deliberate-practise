d1b8c4e6ad06f664e1fb7a36c2282ef3d7a42128
Andy Waite <andyw8@users.noreply.github.com>
2020-06-05 16:21:10 -0400

    Move implementation and test into convential places

--- ruby/fizzbuzz/fizzbuzz_test.rb
+++
@@ -1,9 +0,0 @@
-gem 'minitest', '~> 5.4'
-require 'minitest/autorun'
-require_relative './fizzbuzz'
-
-class FizzbuzzTest < Minitest::Test
-  def test_something
-    fail
-  end
-end
---
+++ ruby/fizzbuzz/test/fizzbuzz_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+require 'fizzbuzz'
+
+class FizzbuzzTest < Minitest::Test
+  def test_addition
+    assert_equal true, false
+  end
+end
